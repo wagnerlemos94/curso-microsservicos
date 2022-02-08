62cef2944256ab817f8e09fbc6e8fbe943b9a0de
wagner lemos <wagner.lemos94@hotmail.com>
2022-02-08 16:34:16 -0300

    hr-payroll implementation mock'

---
+++ hr-payroll/src/main/java/com/devesuperior/hrpayroll/entities/Payment.java
@@ -0,0 +1,21 @@
+package com.devesuperior.hrpayroll.entities;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Payment implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	private Double dailyIncome;
+	private Integer days;
+	
+	public double getTotal() {
+		return days * dailyIncome;
+	}
+
+}
---
+++ hr-payroll/src/main/java/com/devesuperior/hrpayroll/services/PaymentService.java
@@ -0,0 +1,14 @@
+package com.devesuperior.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.devesuperior.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+	
+	public Payment getPayment(Long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+
+}
