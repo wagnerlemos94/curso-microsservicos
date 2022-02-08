d565bd84e418fe971e4d036e373a03155d786d28
wagner lemos <wagner.lemos94@hotmail.com>
2022-02-08 16:52:25 -0300

    ResrTemplate

---
+++ hr-payroll/src/main/java/com/devesuperior/hrpayroll/entities/Worker.java
@@ -0,0 +1,15 @@
+package com.devesuperior.hrpayroll.entities;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@Data
+public class Worker implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private Double dailyIncome;
+	
+}
