795da0afd4d79f689ea13d5fb4a4ba400e639bc3
wagner lemos <wagner.lemos94@hotmail.com>
2022-02-09 14:08:36 -0300

    Project hr-api-gateway-zuul

---
+++ he-api-gateway-zuul/src/main/java/com/devsuperior/heapgatewayzuul/HeApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.heapgatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeApiGatewayZuulApplication.class, args);
+	}
+
+}
