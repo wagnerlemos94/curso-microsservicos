de5d49a7a77a197b3e91cda620c9aac0097e894d
wagner lemos <wagner.lemos94@hotmail.com>
2022-02-10 10:03:12 -0300

    User< ROle, many to many association

---
+++ hr-user/src/main/java/com/devsuperior/hruser/entities/Role.java
@@ -0,0 +1,26 @@
+package com.devsuperior.hruser.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Table(name = "tb_role")
+public class Role implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String roleName;
+
+}
