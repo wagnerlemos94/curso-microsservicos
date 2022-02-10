bb1ca354a5b1de1ca94f5e3b5936db7f5a42863a
wagner lemos <wagner.lemos94@hotmail.com>
2022-02-10 11:05:14 -0300

    Project hr-oauth created

--- hr-oauth/src/test/java/com/devsuperiror/hroauth/HrOauthApplicationTests.java
+++ hr-oauth/src/test/java/com/devsuperiror/hroauth/HrOauthApplicationTests.java
@@ -1,10 +1,10 @@
-package com.devsuperior.hruser;
+package com.devsuperiror.hroauth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HrUserApplicationTests {
+class HrOauthApplicationTests {
 
 	@Test
 	void contextLoads() {
