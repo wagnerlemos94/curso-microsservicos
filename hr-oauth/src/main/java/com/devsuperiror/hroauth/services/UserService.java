e86a3c4ff29ecf5777d44795b53705804f1bd808
wagner lemos <wagner.lemos94@hotmail.com>
2022-02-10 15:46:47 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/devsuperiror/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/devsuperiror/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.devsuperiror.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.devsuperiror.hroauth.entities.User;
 import com.devsuperiror.hroauth.feignclients.UserFeingClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 	
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
@@ -22,6 +25,17 @@ public class UserService {
 			throw new IllegalArgumentException("Email not fund");
 		}else {
 			logger.info("Email found: " + email);
+			return user;
+		}
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeingClient.findByEmail(username).getBody();
+		if(username == null) {
+			throw new UsernameNotFoundException("Email not fund");
+		}else {
+			logger.info("Email found: " + username);
 			return user;
 		}
 	}
